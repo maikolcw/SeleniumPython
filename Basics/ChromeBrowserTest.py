e5548c46f21229b52a84b6be0d03de2b9258c4f5
maikolcw <whysoraccoony@gmail.com>
2023-06-01 19:36:58 -0700

    chrome and firefox v4 webdriver syntax

---
+++ Basics/ChromeBrowserTest.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+# Selenium v4 syntax
+chrome_service = Service("C:\\Users\\Myo\\Documents\\SeleniumWebDrivers\\chromedriver.exe")
+chrome_driver = webdriver.Chrome(service=chrome_service)
+
+chrome_driver.get("https://www.google.com/")
+# maximizes browser window
+chrome_driver.maximize_window()
+# grab and print browser title
+print(chrome_driver.title)
+# closes current browser
+chrome_driver.close()
+# closes all browsers
+# chrome_driver.quit()
---
+++ Basics/FirefoxBrowserTest.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+
+# Selenium v4 syntax
+options = Options()
+# To get firefox binary
+options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+firefox_service = Service("C:\\Users\\Myo\\Documents\\SeleniumWebDrivers\\geckodriver.exe")
+firefox_driver = webdriver.Firefox(service=firefox_service, options=options)
+
+firefox_driver.get("https://www.google.com/")
+# maximizes browser window
+firefox_driver.maximize_window()
+# grab and print browser title
+print(firefox_driver.title)
+# closes current browser
+firefox_driver.close()
+# closes all browsers
+# firefox_driver.quit()
