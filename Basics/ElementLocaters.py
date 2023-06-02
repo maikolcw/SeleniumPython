4bda433843a43bd6601d98fa3e33af47f05ef0b4
maikolcw <whysoraccoony@gmail.com>
2023-06-02 14:54:10 -0700

    updated element locaters

--- Basics/ElementLocaters.py
+++ Basics/ElementLocaters.py
@@ -10,8 +10,8 @@ c_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install(
 c_driver.get("https://www.google.com/")
 c_driver.maximize_window()
 
-# Find search bar element by class name and store in variable search_bar
-search_bar = c_driver.find_element(By.CLASS_NAME, "gLFyf")
+# Find search bar element by Name
+search_bar = c_driver.find_element(By.NAME, "q")
 # Type "Selenium" in search bar
 search_bar.send_keys("Selenium")
 # Wait to see results
@@ -22,11 +22,11 @@ search_bar.send_keys(Keys.ENTER)
 time.sleep(2)
 
 
-# Find search bar element by id
+# Find search bar element by ID
 search_bar = c_driver.find_element(By.ID, "APjFqb")
 # Clear search bar
 search_bar.clear()
-search_bar.send_keys("Chrome")
+search_bar.send_keys("ID")
 time.sleep(2)
 search_bar.send_keys(Keys.ENTER)
 
@@ -46,6 +46,40 @@ search_bar.send_keys("CSSSelector")
 time.sleep(2)
 search_bar.send_keys(Keys.ENTER)
 time.sleep(2)
+
+# Find link element by Link Text
+css_functions_link = c_driver.find_element(By.LINK_TEXT, "CSS Functions")
+# Click element
+css_functions_link.click()
+time.sleep(2)
+
+# Find link element by Partial Link Text
+attr_link = c_driver.find_element(By.PARTIAL_LINK_TEXT, "attr")
+attr_link.click()
+time.sleep(5)
+c_driver.get("https://www.google.com/")
+time.sleep(2)
+
+
+# Find search bar element by Class Name
+search_bar = c_driver.find_element(By.CLASS_NAME, "gLFyf")
+search_bar.send_keys("Class Name")
+time.sleep(2)
+search_bar.send_keys(Keys.ENTER)
+time.sleep(2)
+
+# Find search bar element by Tag Name
+search_bar = c_driver.find_element(By.TAG_NAME, "textarea")
+search_bar.clear()
+search_bar.send_keys("Tag Name")
+
+# Find more than one element
+a_elements = c_driver.find_elements(By.TAG_NAME, "a")
+# Prints the text of first ten links
+for link in a_elements[:10]:
+    print(link.text)
+time.sleep(2)
+
 
 # Tear down
 c_driver.close()
