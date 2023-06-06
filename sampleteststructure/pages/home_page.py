92497750c2c006a098e2e892d22169be2d2bf973
maikolcw <whysoraccoony@gmail.com>
2023-06-05 22:12:36 -0700

    added traveller adult add button test

--- sampleteststructure/pages/home_page.py
+++ sampleteststructure/pages/home_page.py
@@ -14,6 +14,11 @@ class HomePage(BaseDriver):
     DESTINATION_SEARCH_BAR = "//input[@id='location-field-destination']"
     TRAVELLERS_LINK = "//button[normalize-space()='1 room, 2 travellers']"
     TRAVELLERS_ADULT_COUNT_DEFAULT = "//input[@id='adult-input-0']"
+    TRAVELLERS_ADULT_COUNT_DEFAULT_INCREASE_BUTTON = "//div[@class='uitk-layout-flex " \
+                                                     "uitk-layout-flex-align-items-center " \
+                                                     "uitk-layout-flex-justify-content-space-between uitk-step-input " \
+                                                     "adultStepInput uitk-step-input-mounted']//button[2]//span[1] "
+    TRAVELLERS_DONE_BUTTON = "//button[normalize-space()='Done']"
 
     # Attributes
     SEARCH_BAR_ATTRIBUTE = "aria-label"
@@ -31,6 +36,15 @@ class HomePage(BaseDriver):
     def return_travellers_text(self):
         return self.driver.find_element(By.XPATH, self.TRAVELLERS_LINK).text
 
-    def return_adult_count_default(self):
+    def open_travellers_and_return_default_adult_count(self):
         self.driver.find_element(By.XPATH, self.TRAVELLERS_LINK).click()
         return self.driver.find_element(By.XPATH, self.TRAVELLERS_ADULT_COUNT_DEFAULT).get_attribute(self.ADULT_COUNT)
+
+    def return_default_adult_count(self):
+        return self.driver.find_element(By.XPATH, self.TRAVELLERS_ADULT_COUNT_DEFAULT).get_attribute(self.ADULT_COUNT)
+
+    def close_travellers_drop_down(self):
+        self.driver.find_element(By.XPATH, self.TRAVELLERS_DONE_BUTTON).click()
+
+    def click_default_adult_traveller_add_button(self):
+        self.driver.find_element(By.XPATH, self.TRAVELLERS_ADULT_COUNT_DEFAULT_INCREASE_BUTTON).click()
--- sampleteststructure/testcases/test_expedia_stays_module.py
+++ sampleteststructure/testcases/test_expedia_stays_module.py
@@ -25,7 +25,15 @@ class TestExpediaStaysModule():
         print(text)
         assert text == "1 room, 2 travellers"
 
-    def test_travellers_default_adult_count(self):
-        text = self.home_page.return_adult_count_default()
+    def test_default_adult_traveller_count(self):
+        text = self.home_page.open_travellers_and_return_default_adult_count()
+        self.home_page.close_travellers_drop_down()
         print(text)
         assert text == "2"
+
+    def test_default_add_adult_traveller(self):
+        initial_count = self.home_page.open_travellers_and_return_default_adult_count()
+        self.home_page.click_default_adult_traveller_add_button()
+        final_count = self.home_page.return_default_adult_count()
+        print("% s + 1 == % s" % (initial_count, final_count))
+        assert int(initial_count) + 1 == int(final_count)
