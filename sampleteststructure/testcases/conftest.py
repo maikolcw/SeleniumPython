36c5c47115b5305b8587274a8154b5fd432f040b
maikolcw <whysoraccoony@gmail.com>
2023-06-06 18:57:34 -0700

    add documentation for adding more command options and removing url hardcode

--- sampleteststructure/testcases/conftest.py
+++ sampleteststructure/testcases/conftest.py
@@ -9,9 +9,9 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
 
-# request is used to deliver web driver to test method that uses this fixture
+# request is used to deliver web driver to tests that uses this fixture
 @pytest.fixture(scope="class")
-def expedia_setup(request, browser):
+def expedia_setup(request, browser, url):
     # From command line you can select browser type ex. --browser firefox
     # You can add more like Edge, Chromium, and Brave, this is just an example
     if browser == "firefox":
@@ -21,6 +21,8 @@ def expedia_setup(request, browser):
     else:
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.get("https://www.expedia.ca/")
+    # We can remove hardcode of URL
+    # driver.get(url)
     driver.maximize_window()
     # Pass webdriver to request class
     request.cls.driver = driver
@@ -31,9 +33,18 @@ def expedia_setup(request, browser):
 # Add custom options in command line
 def pytest_addoption(parser):
     parser.addoption("--browser")
+    # You can also add more command options like this
+    # In this case we can enter our URL from command line
+    parser.addoption("--url")
 
 
 # Helper fixture that returns command line value of --browser option
 @pytest.fixture(scope="class")
 def browser(request):
     return request.config.getoption("--browser")
+
+
+# Add another command option
+@pytest.fixture(scope="class")
+def url(request):
+    return request.config.getoption("--url")
